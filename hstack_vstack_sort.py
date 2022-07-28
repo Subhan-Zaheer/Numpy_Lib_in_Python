44fd17dca5a2c7cd7bb1a8de8f6ebc9ebeb473d4
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-07-28 15:45:34 +0500

    Some basic example of hstack vstack and sort function

---
+++ hstack_vstack_sort.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+if __name__ == '__main__':
+    a = np.round(10*np.random.rand(2, 4))
+    print(a)
+    b = np.round(10*np.random.rand(2, 4))
+    print(b)
+    c = np.hstack((a, b))   # Will concatenate two arrays horizontally.
+    print(c)
+    d = np.vstack((a, b))   # Will concatenate two arrays vertically.
+    print(d)
+    d.sort(axis=0)   # This will sort the 2 d array by rows.
+    d.sort(axis=1)   # This will sort the 2 d array by columns.
+    print(d)
+    str1 = np.array(['d', 'a', 'c', 'b'])
+    str1.sort()
+    print(str1)
+    print(str1[::-1])
