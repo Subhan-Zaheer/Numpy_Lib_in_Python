89b2469e7a53a19be22fd75ab034e263d3539797
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-08-02 21:46:52 +0500

    Some other commenting added like details about logical operator in numpy arrays

--- func_in_Numpy.py
+++ func_in_Numpy.py
@@ -4,4 +4,12 @@ There are hundreds of functions in Numpy but few are discussed below.
 2. Array manipulation : np.reshape, np.concatenate, np.stack, np.split
 3. Linear Algebra : np.matmul, np.transpose, np.eigvals, np.dot
 4. Statistics : np.mean, np.median, np.max, np.std
+
+
+Logical Operators in Numpy arrays.
+ ==
+ !=
+ >=
+ <=
+    All these operators will produce a result after applying these operators one by one on each element.
 """
