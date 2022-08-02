7518a039430152b1da591e357a39ee1bdaee223b
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-08-02 21:39:02 +0500

    Some basic functions provided by Numpy

---
+++ func_in_Numpy.py
@@ -0,0 +1,7 @@
+"""
+There are hundreds of functions in Numpy but few are discussed below.
+1. Mathematics : np.sum, np.exp, np.round, arithmatic operators.
+2. Array manipulation : np.reshape, np.concatenate, np.stack, np.split
+3. Linear Algebra : np.matmul, np.transpose, np.eigvals, np.dot
+4. Statistics : np.mean, np.median, np.max, np.std
+"""
